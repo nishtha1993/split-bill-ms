9660d056d684e52efb32879c6145627f22f53c4c
nishtha <malhotra.nishtha17@gmail.com>
2024-03-16 13:23:58 +0800

    adding aws_vpc setup with github actions

---
+++ Terraform-VPC/modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+    description = "VPC CIDR Range"
+    type = string
+  
+}
+
+variable "subnet_cidr" {
+    description = "Subnet CIDRS"
+    type = list(string)
+}
+
+variable "subnet_names" {
+    description = "Subnet Names"
+    type = list(string)
+    default = [ "PublicSubnet1","PublicSubnet2" ]
+  
+}
