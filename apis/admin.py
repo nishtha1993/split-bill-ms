86fbdbb96b69b705cee5638243573fa7beac38ac
Parashara <parasharar@gmail.com>
2024-03-18 17:14:43 +0800

    adding some debug logs to see what happens after deploying in aws

--- apis/admin.py
+++ apis/admin.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
+import logging
 
 admin_bp = Blueprint('admin', __name__)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @admin_bp.route("/health")
 def health_check():
     """Hello word method."""
+    logger.info("[GET /admin/health].health_check : Just checking health!")
     return "I am ready to Split-a-bill!!"
 
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 # this will contain the main code which wraps together all code/routes
-
+import logging
 from flask import Flask
 from apis.admin import admin_bp
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 split_bill_app = Flask(__name__)
 
 #create mappings
 split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
+
+@split_bill_app.route('/', methods=['GET'])
+def entry():
+    logger.info('[GET / ].entry : Entered the main entry endpoint')
+    return 'Time to split!'
 
 # run the app.
 if __name__ == "__main__":
