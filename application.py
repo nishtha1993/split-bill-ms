b2a58f3cc5962e4abd240b8154140c2c9d89d579
Parashara <parasharar@gmail.com>
2024-03-27 13:24:58 +0800

    adding flask-cors to ensure that CORS issues dont arise during the course of development

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 # this will contain the main code which wraps together all code/routes
 import logging
 from flask import Flask
+from flask_cors import CORS
 from apis.admin import admin_bp
 
 # Configure logging
@@ -9,6 +10,9 @@ logger = logging.getLogger(__name__)
 
 # Defining the app instance
 split_bill_app = Flask(__name__)
+
+# Enabling cross-origin requests from any site
+CORS(split_bill_app)
 
 #create mappings here
 split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
