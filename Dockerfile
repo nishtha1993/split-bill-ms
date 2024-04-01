b7b08d0855570eeaa50e27993156c80533b16851
nishtha <malhotra.nishtha17@gmail.com>
2024-04-01 14:36:58 +0800

    update flask cors

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Set base image (host OS)
+FROM python:3.8-alpine
+
+# By default, listen on port 5000
+EXPOSE 8080/tcp
+
+# Set the working directory in the container
+WORKDIR /split-bill-ms
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# Install any dependencies
+RUN pip install -r requirements.txt
+
+# Copy the content of the local src directory to the working directory
+COPY application.py .
+
+# Specify the command to run on container start
+CMD ["python", "./application.py"]  
--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 import logging
 from flask import Flask
 from flask_cors import CORS
-from apis.admin import admin_bp
+#from apis.admin import admin_bp
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -11,12 +11,22 @@ logger = logging.getLogger(__name__)
 # Defining the app instance
 split_bill_app = Flask(__name__)
 
+#create mappings here
+#split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
+
 # Enabling cross-origin requests from any site
 CORS(split_bill_app)
 
-#create mappings here
-split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
+logging.getLogger('flask_cors').level = logging.INFO
+
+@split_bill_app.route("/health")
+def health():
+   return "I am alive and healthy!"
+
+@split_bill_app.route("/")
+def hello_world():
+   return "Hello, World!"
 
 # run the app.
 if __name__ == "__main__":
-    split_bill_app.run(debug=True, port=8080)
+    split_bill_app.run(debug=True,host='0.0.0.0',port=8080)
