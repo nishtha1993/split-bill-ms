c948d7e9d67f9efebfea0ee4a7811bd9670ad06c
Parashara <parasharar@gmail.com>
2024-04-08 14:55:59 +0800

    started with friends skeleton

--- apis/friend.py
+++ apis/friend.py
@@ -6,13 +6,13 @@ friend_bp = Blueprint('user', __name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-@friend_bp.route('/', methods=['GET'])
-def entry():
-    logger.info('[GET /admin ].entry : Entered the main entry endpoint')
-    return 'Time to split!'
+'''
+Base Url : /friend (see application.py)
 
-@friend_bp.route("/health")
-def health_check():
-    """Hello word method."""
-    logger.info("[GET /admin/health].health_check : Just checking health!")
-    return "I am alive!"
+Apis to implement here:
+1. /getMyFriends
+2. /getFriendHistory
+3. /settleWithHistory
+4. /nudge
+
+'''
