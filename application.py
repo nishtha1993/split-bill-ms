c37c640ec63baf7e3a9e4c1a8299ced16266afdd
Parashara <parasharar@gmail.com>
2024-04-08 10:52:06 +0800

    initial commit for creating skeleton code

--- application.py
+++ application.py
@@ -1,11 +1,9 @@
 # this will contain the main code which wraps together all code/routes
 import logging
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-import boto3
-import os
 
-# from apis.admin import admin_bp
+from apis.admin import admin_bp
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -15,45 +13,11 @@ logger = logging.getLogger(__name__)
 split_bill_app = Flask(__name__)
 
 # create mappings here
-# split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
+split_bill_app.register_blueprint(admin_bp, url_prefix='/admin')
 
 # Enabling cross-origin requests from any site
 CORS(split_bill_app)
-
 logging.getLogger('flask_cors').level = logging.INFO
-
-
-session = boto3.Session()
-    # aws_access_key_id=os.environ.get('secrets.AWS_ACCESS_KEY'),
-    # aws_secret_access_key=os.environ.get('secrets.AWS_SECRET_ACCESS_KEY')
-# )
-
-dynamodb = session.resource('dynamodb', region_name='us-east-1')
-table = dynamodb.Table('Users')
-
-
-@split_bill_app.route("/health")
-def health():
-    logger.info("Inside /health and it works correctly!")
-    return "I am alive and healthy!"
-
-
-@split_bill_app.route("/")
-def hello_world():
-    return "Hello, World!"
-
-
-@split_bill_app.route("/getUserById")
-def get_items():
-    logger.info("Inside /health and it works correctly!")
-    response = table.get_item(
-        Key={
-            'userId': '1'
-        }
-    )
-    logger.info("Got the response from dynamo")
-    return jsonify(response)
-
 
 # run the app.
 if __name__ == "__main__":
