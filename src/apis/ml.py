6137b8be18fb2c9d3c43d4c8294b430822e7b24f
nishtha <malhotra.nishtha17@gmail.com>
2024-04-19 23:31:45 +0800

    Add textract extraction of invoice

--- src/apis/ml.py
+++ src/apis/ml.py
@@ -5,6 +5,8 @@ import json
 from json import dumps, loads
 import logging
 from marshmallow import ValidationError
+from collections import defaultdict
+from urllib.parse import unquote_plus
 
 
 ml_bp = Blueprint('ml', __name__)
@@ -38,37 +40,83 @@ and we can use these categories in the graph section later on
 
 
 '''
-def extract_invoice_details(textract_response):
-    # For example, you can parse the response for specific key-value pairs or patterns
-    # and return them as a dictionary
-    logger.info(textract_response)
-    return {
-        'invoice_number': 'INV12345',
-        'total_amount': '$100.00',
-        'date': '2024-04-13'
-    }
 
 @ml_bp.route('/parseFromTextract', methods=['POST'])
 def parseFromTextract():
     request_guid = create_random_guid()
     logger.info(
-        f'[POST /ml/parseFromTextract] | RequestId: {request_guid}.'
+        f'[POST /ml/parseFromTextract] | RequestId: {request_guid} for file.'
     )
     
     try:
-        file = request.files['file']
-        logger.info(
-        f'[POST /ml/parseFromTextract] | File: {file}.'
-        )
-        response = textract_client.detect_document_text(
-        Document={
-            'Bytes': file.read()
-        }
-    )
+        key_map, value_map, block_map = get_kv_map("split-bill-receipts", "invoice2.png")
+
+        # Get Key Value relationship
+        kvs = get_kv_relationship(key_map, value_map, block_map)
+        print("\n\n== FOUND KEY : VALUE pairs ===\n")
+
+        for key, value in kvs.items():
+            print(key, ":", value)
+        
         
     except ValidationError as err:
         # Return a nice message if validation fails
         return jsonify(err.messages), 400
-    details = extract_invoice_details(response)
-    return details
+    
+    return kvs
 
+def get_kv_map(bucket, key):
+    # process using image bytes
+    response = textract_client.analyze_document(Document={'S3Object': {'Bucket': bucket, "Name": key}}, FeatureTypes=['FORMS'])
+
+    # Get the text blocks
+    blocks = response['Blocks']
+
+    # get key and value maps
+    key_map = {}
+    value_map = {}
+    block_map = {}
+    for block in blocks:
+        block_id = block['Id']
+        block_map[block_id] = block
+        if block['BlockType'] == "KEY_VALUE_SET":
+            if 'KEY' in block['EntityTypes']:
+                key_map[block_id] = block
+            else:
+                value_map[block_id] = block
+
+    return key_map, value_map, block_map
+
+
+def get_kv_relationship(key_map, value_map, block_map):
+    kvs = defaultdict(list)
+    for block_id, key_block in key_map.items():
+        value_block = find_value_block(key_block, value_map)
+        key = get_text(key_block, block_map)
+        val = get_text(value_block, block_map)
+        kvs[key].append(val)
+    return kvs
+
+
+def find_value_block(key_block, value_map):
+    for relationship in key_block['Relationships']:
+        if relationship['Type'] == 'VALUE':
+            for value_id in relationship['Ids']:
+                value_block = value_map[value_id]
+    return value_block
+
+
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] == 'SELECTED':
+                            text += 'X'
+
+    return text
